d910aaf2234c576f2143508961ba651f4624d8ad
victordelajarte <29466815+victordelajarte@users.noreply.github.com>
2024-02-26 12:13:01 +0100

    feat: update config to test the whole file

--- index.js
+++ index.js
@@ -19,14 +19,40 @@ const pdfDoc = await PDFDocument.load(formPdfBytes);
 const specialFont = await pdfDoc.embedFont(StandardFonts.TimesRomanBoldItalic);
 
 const config = {
-  company_name: { text: "My Company" },
-  contact_email: { text: "victor@gmail.com" },
-  contact_signature: {
-    font: specialFont,
-    text: "Victor de Lajarte",
-  },
-  start_date: { text: new Date().toLocaleDateString() },
-};
+  contact_full_name: { text: "Chuck Norris" },
+  contact_email: { text: "contact@email.com" },
+  contact_phone_number: { text: "+33601020304" },
+  talent_full_name: { text: "Arya Stark" },
+  talent_qualification: { text: "Technicien spécialisé en rien" },
+  talent_email: { text: "arya.stark@winterfell.com" },
+  talent_phone_number: { text: "+33610203040" },
+  company_name: { text: "Company name - Agency name" },
+  contact_signature: { font: specialFont, text: "Chuck Norris" },
+  talent_signature: { font: specialFont, text: "Arya Stark" },
+  total_day_hours: { text: "32h 30min" },
+  total_night_hours: { text: "5h 15min" },
+  monday_date: { text: "01/01" },
+  tuesday_date: { text: "02/01" },
+  wednesday_date: { text: "03/01" },
+  thursday_date: { text: "04/01" },
+  friday_date: { text: "05/01" },
+  saturday_date: { text: "06/01" },
+  sunday_date: { text: "07/01" },
+  month: { text: "janvier" },
+  week: { text: "1" },
+  start_date: { text: "01/01" },
+  end_date: { text: "07/01" },
+  monday_day_hours: { text: "8h 00min" },
+  monday_night_hours: { text: "-" },
+  tuesday_day_hours: { text: "7h 00min" },
+  tuesday_night_hours: { text: "0h 30min" },
+  wednesday_day_hours: { text: "6h 00min" },
+  wednesday_night_hours: { text: "1h 00min" },
+  thursday_day_hours: { text: "5h 30min" },
+  thursday_night_hours: { text: "1h 30min" },
+  friday_day_hours: { text: "6h 00min" },
+  friday_night_hours: { text: "2h 15min" }
+}
 
 // Get the form so we can add fields to it
 const form = pdfDoc.getForm();
